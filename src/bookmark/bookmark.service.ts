b0b6c01b0097eaa46a95a410958763892d2def0e
chandrika chenna <chandrika.c@codemonk.in>
2024-09-10 07:15:56 +0530

    feat: service for bookmarks

---
+++ src/bookmark/bookmark.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { BookmarkSchema } from './bookmark.schema';
+import mongoose from 'mongoose';
+
+@Injectable()
+export class BookmarkService {
+  constructor(
+    @InjectModel(BookmarkSchema.name)
+    private bookModel: mongoose.Model<BookmarkSchema>,
+  ) {}
+
+  async findAll(): Promise<BookmarkSchema[]> {
+    const books = await this.bookModel.find();
+    return books;
+  }
+}
