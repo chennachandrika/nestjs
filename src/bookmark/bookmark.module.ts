e1d1c092e1ede90aa40f3f34586ad895bb85f1c0
chandrika chenna <chandrika.c@codemonk.in>
2024-09-10 07:16:23 +0530

    feat: bookmark module

--- src/bookmark/bookmark.module.ts
+++ src/bookmark/bookmark.module.ts
@@ -1,4 +1,16 @@
 import { Module } from '@nestjs/common';
+import { BookmarkService } from './bookmark.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { BookmarkSchema, BookmarkSchemaFactory } from './bookmark.schema';
+import { BookmarkController } from './bookmark.controller';
 
-@Module({})
+@Module({
+  imports: [
+    MongooseModule.forFeature([
+      { name: 'BookmarkSchema', schema: BookmarkSchemaFactory },
+    ]),
+  ],
+  controllers: [BookmarkController],
+  providers: [BookmarkService],
+})
 export class BookmarkModule {}
